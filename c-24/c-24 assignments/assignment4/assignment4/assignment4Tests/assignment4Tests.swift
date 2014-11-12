b725fa89f17bd3a7c97d8f37e95109394c786b7b
quadstreaker <scott@quadstreaker.com>
2014-11-12 11:59:22 -0800

    set up for assignment 4

---
+++ c-24/c-24 assignments/assignment4/assignment4/assignment4Tests/assignment4Tests.swift
@@ -0,0 +1,36 @@
+//
+//  assignment4Tests.swift
+//  assignment4Tests
+//
+//  Created by Scott Kendall on 11/12/14.
+//  Copyright (c) 2014 Scott Kendall. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class assignment4Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
